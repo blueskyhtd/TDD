1032c658e19e184fe9bdc444081af6dd7fff357a
unknown <hadtt17@VIETTELGROUP.VIETTELAD.COM>
2016-08-12 15:33:57 +0700

    update test class

--- TDDSimple/src/main/java/com/viettel/app/Application.java
+++ TDDSimple/src/main/java/com/viettel/app/Application.java
@@ -1,5 +1,13 @@
 package com.viettel.app;
 
+import org.junit.runner.JUnitCore;
+
+import com.viettel.app.test.TestSuite;
+
 public class Application {
 
+	public static void main(String[] arg) {
+		JUnitCore junit = new JUnitCore();
+		junit.run(TestSuite.class);
+	}
 }
